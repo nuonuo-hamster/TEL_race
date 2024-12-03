2e7760d49ffa98e205420be0cfc18d8a6f389951
nuonuo-hamster <qazart123@gmail.com>
2024-12-04 03:07:47 +0800

    .sh

--- launch_client.bat
+++ launch_client.bat
@@ -1,7 +1,9 @@
 @echo off
 echo Executing Python scripts...
 
+@REM buttom
 start C:/Users/superuser/.virtualenvs/2024_TEL-tUU5St8O/Scripts/python.exe d:/TEL2024_Intergral/ClientControl_button.py
+@REM wheel
 start C:/Users/superuser/.virtualenvs/2024_TEL-tUU5St8O/Scripts/python.exe d:/TEL2024_Intergral/ClientControl_wheel.py
 
 echo All scripts have been executed.
