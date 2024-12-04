9c8a0f75c8de07bd8a7a937b52674f9059de919c
ical <nukical@gmail.com>
2024-12-04 20:40:51 +0800

    12/04 20:40

--- launch_client.bat
+++ launch_client.bat
@@ -2,9 +2,9 @@
 echo Executing Python scripts...
 
 @REM 設置 IP 並寫入 ip.txt
-echo ip='192.168.0.222' > d:/TEL2024_Intergral/ip.txt
+echo ip='192.168.0.119' > d:/TEL2024_Intergral/ip.txt
 @REM d435i client
-start C:/Users/superuser/.virtualenvs/2024_TEL-tUU5St8O/Scripts/python.exe d:/TEL2024_Intergral/D435iClient.py
+@REM start C:/Users/superuser/.virtualenvs/2024_TEL-tUU5St8O/Scripts/python.exe d:/TEL2024_Intergral/D435iClient.py
 @REM buttom client
 start C:/Users/superuser/.virtualenvs/2024_TEL-tUU5St8O/Scripts/python.exe d:/TEL2024_Intergral/ClientControl_button.py
 @REM wheel client
